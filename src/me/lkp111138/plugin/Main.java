d134ec5f5d56c7affcca0ca07f091cae1b0bcbb6
UtopiaMint <admin@lkp111138.me>
2020-05-01 20:43:35 +0800

    Moved config for modules in separate files

--- src/me/lkp111138/plugin/Main.java
+++ src/me/lkp111138/plugin/Main.java
@@ -3,16 +3,18 @@ package me.lkp111138.plugin;
 import me.lkp111138.plugin.chestgui.ChestGuiEventListener;
 import me.lkp111138.plugin.command.*;
 import me.lkp111138.plugin.item.CustomItem;
-import me.lkp111138.plugin.rpg.CustomMob;
-import me.lkp111138.plugin.rpg.CustomMobEventListener;
 import me.lkp111138.plugin.npc.NPC;
 import me.lkp111138.plugin.npc.NPCEventListener;
 import me.lkp111138.plugin.npc.task.TrackingTask;
+import me.lkp111138.plugin.rpg.CustomMob;
+import me.lkp111138.plugin.rpg.CustomMobEventListener;
 import me.lkp111138.plugin.rpg.task.HealthBarTask;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -27,26 +29,31 @@ public class Main extends JavaPlugin {
         FileConfiguration config = this.getConfig();
         config.set("cute", true);
         // npc
-        List<Map<?, ?>> npcSection = config.getMapList("npc");
+        FileConfiguration npcConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "npc.yml"));
+        List<Map<?, ?>> npcSection = npcConfig.getMapList("npc");
         npcSection.forEach(map -> {
             NPC npc = new NPC(map);
             if ((Boolean) ((Map) map).getOrDefault("spawned", false)) {
                 System.out.println(String.format("Spawning NPC #%d at %s %s", npc.getId(), npc.getLocation(), npc.spawn()));
             }
         });
-        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TrackingTask(), 0, 5);
-        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new HealthBarTask(), 0, 2);
         // custom items
-        ConfigurationSection itemSection = config.getConfigurationSection("item");
+        FileConfiguration itemConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "items.yml"));
+        ConfigurationSection itemSection = itemConfig.getConfigurationSection("items");
         for (String key : itemSection.getKeys(false)) {
             new CustomItem(key, itemSection.getConfigurationSection(key));
         }
         // custom mobs
-        ConfigurationSection mobSection = config.getConfigurationSection("mob");
+        FileConfiguration mobConfig = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "mobs.yml"));
+        ConfigurationSection mobSection = mobConfig.getConfigurationSection("mobs");
         for (String key : mobSection.getKeys(false)) {
             new CustomMob(key, mobSection.getConfigurationSection(key));
         }
         saveConfig();
+
+        // repeating tasks
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TrackingTask(), 0, 5);
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new HealthBarTask(), 0, 2);
 
         // commands
         this.getCommand("fdnol").setExecutor(new CommandTest());
